1d94b45e03a3b183ff888291bf0fdb93cae7e436
AamirSohail07000 <sohail.aamir07000@gmail.com>
2024-10-10 11:11:15 +0530

    Initial commit

--- url_shortener/app.py
+++ url_shortener/app.py
@@ -1,2 +1,14 @@
 from flask import Flask
-import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  return "Hello World"
+
+@app.route('/aamir')
+def heelo_aamir():
+  return "Hi Aamir"
+
+if __name__ == '__main__':
+  app.run(port=5000, debug=True)
